e07db730b630f37a17532a6f1ca42cb03001e339
Izzy Lancaster <avrisaac555@gmail.com>
2021-05-03 00:13:52 -0700

    Using new i3 conifg

--- home.nix
+++ home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports = [ ./theme.nix ];
+  imports = [ ./theme.nix ./home-window-manager.nix ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -94,14 +94,6 @@
     setXresources = true;
   };
 
-  # Rofi (program launcher) config
-  programs.rofi = {
-    enable = true;
-    package =
-      pkgs.rofi.override { plugins = [ pkgs.rofi-calc pkgs.rofi-emoji ]; };
-    pass.enable = true;
-  };
-
   programs.neovim = {
     enable = true;
     withPython3 = true;
@@ -130,143 +122,4 @@
     };
   };
 
-  xsession = {
-    enable = true;
-    pointerCursor = {
-      name = "Bibata_Classic";
-      package = pkgs.bibata-cursors;
-    };
-    windowManager.i3 = {
-      enable = true;
-      config = let
-        zipToAttrs = lib.zipListsWith (n: v: { ${n} = v; });
-        mergeAttrList = lib.foldr lib.mergeAttrs { };
-        mergeAttrMap = f: l: mergeAttrList (lib.concatMap f l);
-
-        modifier = "Mod4";
-        arrowKeys = [ "Left" "Down" "Up" "Right" ];
-        viKeys = [ "h" "j" "k" "l" ];
-        workspaceNames = builtins.map toString (lib.range 1 40);
-        workspaceKeys = lib.crossLists (m: k: "${m}${k}") [
-          [ "" "Ctrl+" "Mod1+" "Ctrl+Mod1+" ]
-          [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" ]
-        ];
-
-        fonts = [ "monospace 10" ];
-
-        dirNames = [ "left" "down" "up" "right" ];
-        resizeActions =
-          [ "shrink width" "grow height" "shrink height" "grow width" ];
-        mode_system =
-          "System (l) lock, (e) logout, (s) suspend, (h) hibernate, (r) reboot, (Shift+s) shutdown";
-      in {
-        bars = [{
-          inherit fonts;
-          position = "bottom";
-         #  statusCommand =
-         #   "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
-        }];
-        inherit fonts;
-        startup = [{
-          command = "./.screenlayout/desktop.sh";
-          notification = false;
-        }];
-        keybindings = mergeAttrList [
-          (mergeAttrMap (ks:
-            zipToAttrs (map (k: "${modifier}+${k}") ks)
-            (map (d: "focus ${d}") dirNames)) [ viKeys arrowKeys ])
-          (mergeAttrMap (ks:
-            zipToAttrs (map (k: "${modifier}+Shift+${k}") ks)
-            (map (d: "move ${d}") dirNames)) [ viKeys arrowKeys ])
-          (mergeAttrMap (ks:
-            zipToAttrs (map (k: "${modifier}+Ctrl+${k}") ks)
-            (map (d: "move container to output ${d}") dirNames)) [
-              viKeys
-              arrowKeys
-            ])
-          (mergeAttrMap (ks:
-            zipToAttrs (map (k: "${modifier}+Ctrl+Shift+${k}") ks)
-            (map (d: "move workspace to output ${d}") dirNames)) [
-              viKeys
-              arrowKeys
-            ])
-          (mergeAttrList (zipToAttrs (map (k: "${modifier}+${k}") workspaceKeys)
-            (map (d: "workspace ${d}") workspaceNames)))
-          (mergeAttrList
-            (zipToAttrs (map (k: "${modifier}+Shift+${k}") workspaceKeys)
-              (map (d: "move workspace ${d}") workspaceNames)))
-          {
-            "${modifier}+Return" = "exec urxvt";
-            "${modifier}+Shift+Return" =
-              "exec ${pkgs.enlightenment.terminology}/bin/terminology";
-            "${modifier}+Shift+q" = "kill";
-            "${modifier}+d" = "exec rofi -show run";
-            "${modifier}+Shift+d" = "exec rofi -show drun -modi drun";
-            "${modifier}+c" = "exec rofi -show calc -modi calc";
-            "${modifier}+x" = "exec rofi -show emoji -modi emoji";
-            "${modifier}+n" = "exec rofi -show window -modi window";
-            "${modifier}+p" = "exec rofi-pass";
-            "${modifier}+Shift+s" =
-              "exec ${pkgs.rofi-systemd}/bin/rofi-systemd";
-            "${modifier}+Ctrl+s" = "exec rofi -show ssh -modi ssh";
-
-            "${modifier}+a" = "focus parent";
-
-            "${modifier}+r" = "mode resize";
-            "${modifier}+Pause" = ''mode "${mode_system}"'';
-
-            "${modifier}+g" = "split h";
-            "${modifier}+v" = "split v";
-            "${modifier}+f" = "fullscreen toggle";
-
-            "${modifier}+s" = "layout stacking";
-            "${modifier}+w" = "layout tabbed";
-            "${modifier}+e" = "layout toggle split";
-
-            "${modifier}+Shift+space" = "floating toggle";
-            "${modifier}+space" = "focus mode_toggle";
-
-            "${modifier}+Shift+c" = "reload";
-            "${modifier}+Shift+r" = "restart";
-            "${modifier}+Shift+e" =
-              "exec i3-nagbar -t warning -m 'Do you want to exit i3?' -b 'Yes' 'i3-msg exit'";
-            #"${modifier}+Control+q" = "exec bash -c \"i3-nagbar -t warning -m 'do you want to kill this window $(${pkgs.xdotool}/bin/xdotool selectwindow getwindowpid)\"";
-            "--release ${modifier}+Ctrl+q" =
-              "exec ${pkgs.xorg.xkill}/bin/xkill";
-            "--release ${modifier}+Ctrl+Shift+q" = ''
-              exec kill -9 $(xdotool getwindowpid "$(xdotool getwindowfocus)" )'';
-          }
-        ];
-        modes = {
-          resize = mergeAttrList [
-            (mergeAttrMap
-              (ks: zipToAttrs ks (map (a: "resize ${a}") resizeActions)) [
-                viKeys
-                arrowKeys
-              ])
-            {
-              "Escape" = "mode default";
-              "Return" = "mode default";
-            }
-          ];
-          "${mode_system}" = let
-            lock_command = "i3lock -c 555555";
-            act_then_lock =
-              (act: "${act} ; exec ${lock_command} ; mode default");
-          in {
-            "l" = "exec ${lock_command}; mode default";
-            "e" = "exec i3-msg exit";
-            "s" = act_then_lock "exec systemctl suspend";
-            "h" = act_then_lock "exec systemctl hibernate";
-            "r" = "exec systemctl reboot";
-            "Shift+s" = "exec systemctl poweroff";
-
-            "Return" = "mode default";
-            "Escape" = "mode default";
-          };
-        };
-        inherit modifier;
-      };
-    };
-  };
 }
